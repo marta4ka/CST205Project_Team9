320c9ca937cbeda6e4f7c954353938bf17ec8e09
MRMisterMarco <christup21@gmail.com>
2019-05-11 05:43:13 -0700

    Made some changes and now the program works

--- extract_palette.py
+++ extract_palette.py
@@ -8,7 +8,6 @@ Description:  This file takes an image and analyzes the image to extract its
               horizontally on a blank canvas. When the canvas is created we
               will have two examples: a palette with the colors in the image,
               and the original image with the palette beneath it.
-
 """
 #we first import pillow module so that we can be able to manipulate image
 from PIL import Image, ImageDraw
@@ -16,7 +15,7 @@ from PIL import Image, ImageDraw
 from webcolors import rgb_to_hex
 #This function will be used to get the number of colors in the image
 #so that we can extract each one and create a palette with the specified colors.
-def extract_colors(uploaders_image, outline_width, palette_size, outline_color, color_count=10):
+def extract_colors(uploaders_image, outline_width, palette_length_div, outline_color, color_count=10):
     #we grab the original image that the user uploaded and open it
     initial_image = Image.open(uploaders_image)
     #then we figure out the dimensions of the uploaded image
@@ -32,23 +31,23 @@ def extract_colors(uploaders_image, outline_width, palette_size, outline_color, 
     colors_for_palette.putalpha(0)
     #we create a list of tuples for our colors in the palette and use the getcolor
     #to search for the main colors in the image
-    list_of_colors = colors_for_palette.getcolors(80*80)      # array of colors in the image
+    list_of_colors = colors_for_palette.getcolors(80*80)
     #then we print the type of our color object i.e. <class 'list'>
     print(type(list_of_colors))
     #this value will be used to assign the sampled colors size so we can
     #create the sequence of colors in the palette
-    sampled_colors_size= 100
+    sampled_colors_size = 100
     #and create the dimensions for our palette
-    customized_palette_size = int(height/palette_size)
+    palette_height = int(height/palette_length_div)
     #Here we create a blank canvas that will use to draw(print) the new image with palette
-    canvas = Image.new("RGB", (width, height + customized_palette_size))
-    image_with_palette_caption = Image.new("RGB", (width, customized_palette_size))
+    canvas = Image.new("RGB", (width, height + palette_height))   # blank canvas(original image + palette)
+    image_with_palette_caption = Image.new("RGB", (width, palette_height))
     #and then we create the palette image just by itself
-    palette_for_image = Image.new("RGB", (color_count*sampled_colors_size, sampled_colors_size))
+    palette = Image.new("RGB", (color_count*sampled_colors_size, sampled_colors_size))
     #we use ImageDraw Module to create a new image with our palette displayed
     palette_below_image = ImageDraw.Draw(image_with_palette_caption)
     #and then we use it again to display the palette only
-    pure_palette = ImageDraw.Draw(palette_for_image)
+    pure_palette = ImageDraw.Draw(palette)
     #we give our new images an x coordinate of 0 so that they can be placed
     #on the webpage
     position_image_with_palette = 0
@@ -56,14 +55,13 @@ def extract_colors(uploaders_image, outline_width, palette_size, outline_color, 
     #We reduce the size of the width of the palette to fit our webpage screen
     sampled_palette_size = width/10
     #create a list for our hex values
-    list_of_hex = []
-
+    hex_values = []
     #this loop wil create the palette, it will go through each of the 10 colors
     #and will place each color in a rectangle box and place them all sequentially
     #together in a horizontal direction
-    for amount_of_colors, unique_color in color_palette:
+    for amount_of_colors, unique_color in list_of_colors:
         #create palette to be placed below image
-        palette_below_image.rectangle([position_image_with_palette, 0, position_image_with_palette + sampled_palette_size, customized_palette_size], fill=unique_color, width=outline_width, outline=outline_color)
+        palette_below_image.rectangle([position_image_with_palette, 0, position_image_with_palette + sampled_palette_size, palette_height], fill=unique_color, width=outline_width, outline=outline_color)
         #create the palette that will be displayed at the bottom of the webpage
         pure_palette.rectangle([position_pure_palette, 0, position_pure_palette+sampled_colors_size, sampled_colors_size], fill=unique_color)
         #we specify where to place the image with palette
@@ -71,14 +69,14 @@ def extract_colors(uploaders_image, outline_width, palette_size, outline_color, 
         #and where to place the palette
         position_pure_palette = position_pure_palette + sampled_colors_size
         #display the hex values for each beneath the specified color
-        list_of_hex.append(rgb_to_hex(unique_color[:3]))
+        hex_values.append(rgb_to_hex(unique_color[:3]))
     #we then delete these objects so that the program is refreshed when we go back to main page
     del palette_below_image
     del pure_palette
     #tuple with specified dimensions for one of our images.
-    customized_canvas = (0, height, width, height + customized_palette_size)
+    customized_canvas = (0, height, width, height + palette_height)
     #finally we put the results on the canvas to be displayed
     canvas.paste(initial_image)
     canvas.paste(image_with_palette_caption, customized_canvas)
     #displays the the results i.e. palette and palette with image above it
-    return canvas, palette_for_image, list_of_hex
+    return canvas, palette, hex_values
--- flask_webpage.py
+++ flask_webpage.py
@@ -11,25 +11,24 @@ Description: This file is our main file for our palette converter program. It
              hex values. The second route function will be used to display
              the results.
 """
-from extract_palette import extract_colors
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms import IntegerField
-from wtforms.validators import NumberRange
 from flask import Flask, render_template, flash, redirect, url_for, request, session
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from wtforms.validators import NumberRange
 from werkzeug.utils import secure_filename
-from flask_bootstrap import Bootstrap
+from wtforms import IntegerField
 from webcolors import hex_to_rgb
+from flask_wtf import FlaskForm
+from extract_palette import extract_colors
 import uuid
 import os
 #this is our form for flask. We use it to accept the users uploaded image.
 #this form specifies that we will only accept files that are image files.
 class PhotoForm(FlaskForm):
-    photo = FileField(label='Upload your image', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg'])])
-    height_of_palette = IntegerField('Height of palette', validators=[NumberRange(1, 10)])
-    width_of_palette = IntegerField('Width of palette', validators=[NumberRange(1, 40)])
+    photo = FileField('Upload Image', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg'])])
+    palette_height = IntegerField('Palette Height', validators=[NumberRange(1, 10)])
+    palette_outline_width = IntegerField('Palette Outline Width', validators=[NumberRange(1, 40)])
 #We create an instance of the flask class
 app = Flask(__name__)
 #we create a class that will has the specifications for an uploaded object
@@ -42,20 +41,22 @@ app.config.from_object(Config)
 #we create our decorator to bind our function to the flask url
 #we have both GET and POST because we are asking the user to
 #import i.e.POST an image
-@app.route('/home', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 #this function is where the user will upload the image. When the image is
 #uploaded, it will be read the color values
 def upload_image():
+    #we display our home screen using url1
+    url1 = url_for("static", filename ="images/home.jpg")
     #researched how to use PhotoForm() and found an example at
     #https://pythonhosted.org/Flask-WTF/form.html
     #this provides a form for photo data using the flask_wtf module
-    submitted_image = PhotoForm()
+    form = PhotoForm()
     #if the user uploads an image i.e. checks if upload is an image
-    if submitted_image.validate_on_submit():
+    if form.validate_on_submit():
         #takes the color values of the image and displays them
         print(hex_to_rgb(request.form.get('palette_outline_color')))
         #we store the image data in an object
-        image_info = submitted_image.photo.data
+        image_info = form.photo.data
         #we want to make sure we are getting correct data
         #we secure the filename before storing it directly on the filesystem.
         filename = secure_filename(image_info.filename)
@@ -65,19 +66,19 @@ def upload_image():
         #we update filename to give a unique id to each of the files uploaded.
         #we do this using the os import and generate a random unique hex value
         filename = uuid.uuid4().hex + extension
-        #we display it
+        #we print the uploaded files name
         print(filename)
         #here we take all the info we have gathered and create a pallete (new iimage)
         #that will be displayed below the submitted image. This will show the colors
         #in the submitted image as well as their hex values
-        image_info, palette, hex_values = get_colors(image_info, palette_length_div=submitted_image.height_of_palette.data,
-                                          outline_width=submitted_image.width_of_palette.data, outline_color=hex_to_rgb(request.form.get('palette_outline_color')))
+        image_info, palette, hex_values = extract_colors(image_info, palette_length_div=form.palette_height.data, outline_width=form.palette_outline_width.data,
+                       outline_color=hex_to_rgb(request.form.get('palette_outline_color')))
         #we then create an object that will contain the image from our absolute (root) static directory
         #This makes it easier for us to find the image file
-        image_database = os.path.join(app.root_path, 'static/images', filename)
+        image_database = os.path.join(app.root_path, 'static/images',  filename)
         #palette from image chosen from database
         image_palette = os.path.join(app.root_path, 'static/images', "pal"+filename)
-        #now we add our hex_values to the session dictionary that is connected to app
+        #now we add our list_of_hex to the session dictionary that is connected to app
         session['hex_values'] = hex_values
         #we then save our image_info and its data from image database
         image_info.save(image_database)
@@ -85,37 +86,41 @@ def upload_image():
         palette.save(image_palette)
         #when all the info is validated and organized (as done above), we will redirect
         #and display the uploaded image
-        return redirect(url_for('go_to_image', name=filename, height=image_info.height, width=image_info.width))
+        return redirect(url_for('display_image', name=filename, height=image_info.height, width=image_info.width))
     #we then render our html template
-    return render_template('upload_image.html', form=submitted_image, src='default')
+    return render_template('upload_image.html', form=form, src='default', homepage = url1)
 
 #this decorator will specify that we want to redirect to the image that was uploaded
-@app.route('/go_to_image/<name>/<height>/<width>')
+@app.route('/display_palette/<name>/<height>/<width>')
 #this function is for our second flask webpage that will display the palette
 #with the image
-def display_image(picture, height, width):
+def display_image(name, height, width):
+    #link to the homepage for the user to upload another image
+    url_back = url_for("upload_image")
     #we first get the image so we can display it
     fetch_image = url_for('static', filename='images/' + name)
     #then we get the palette for the image to display it as well
     fetch_image_palette = url_for('static', filename='images/' + "pal" + name)
+    #complimented version
+    #complimented_image = url_for()
     #we specify the dimensions for the returned image
     height, width = adjust_dimensions(int(height), int(width))
     #we render the html template with all the necessary objects that are needed
-    return render_template('display_image.html', src=src, src2=src2, height=height, width=width, hex_values=session.get('hex_values'))
+    return render_template('display_image.html', src=fetch_image, src2=fetch_image_palette, height=height, width=width, hex_values=session.get('hex_values'), back=url_back)
+#we use this function to create a custom height and width
+def adjust_dimensions(height, width):
+    if height < 850 and width < 850:
+        return height, width
+    else:
+        while height > 850 and width > 850:
+            height = int(height/2)
+            width = int(width/2)
+        return height, width
 
-    #maybe put this function in colors and import it
-    def adjust_dimensions(height, width):
-        if height < 850 and width < 850:
-            return height, width
-        else:
-            while height > 850 and width > 850:
-                height = int(height/2)
-                width = int(width/2)
-            return height, width
 #display error message if something goes wrong
 @app.errorhandler(413)
-def error_message(e):
+def error413(e):
     return render_template('413.html'), 413
-
+#here is where we call the application to run it
 if __name__ == '__main__':
     app.run(debug=True)
